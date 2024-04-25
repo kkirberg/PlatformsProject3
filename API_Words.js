af855cc001cf46deab01bfc6400288e1fc2d9813
Christian Carreira <Christian>
2024-04-25 12:46:56 -0400

    API_Words

---
+++ API_Words.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Button } from 'react-native';
+
+export default function API_Words() {
+    //Put API here
+
+    //temp array
+    let words = ["Cat", "Dog", "Mouse", "Bull", "Tiger", "Lion", "Eagle"];
+    return(words);
+}
